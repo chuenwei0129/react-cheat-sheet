0feea2e368bed5816fd283f9281555181359c3e9
chuenwei0129 <chuenwei0129@gmail.com>
2025-05-29 11:41:28 +0800

    feat: 新增页面组件以丰富布局和样式
    - 在 app/css/bfc/html 目录下新增 Page 组件，包含两个不同背景色的 div 元素，提升页面的视觉效果和结构。

---
+++ app/css/bfc/html/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <>
+      <div className="absolute mb-10 h-10 w-full bg-red-500">1</div>
+      <div className="mt-10 h-10 w-full bg-blue-500">2</div>
+    </>
+  )
+}
